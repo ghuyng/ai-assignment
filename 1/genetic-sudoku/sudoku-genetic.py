e6ba9d6f3d068294870e42f3fd8baa21715656ed
daanturo <daanturo@gmail.com>
2021-04-09 21:17:06 +0700

    1.3: Define class Grid

--- 1/genetic-sudoku/sudoku-genetic.py
+++ 1/genetic-sudoku/sudoku-genetic.py
@@ -2,11 +2,20 @@
 
 import csv
 import sys
+import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class Grid:
+    def __init__(self, grid):
+        self.current = grid
+        self.initial = np.copy(grid)
+        self.N = len(grid)
 
 
 def main(args):
-    grid = read_initial(args[1])
-    N = len(grid)
+    grid = Grid(read_initial(args[1]))
 
 
 def read_initial(input_file):
@@ -20,6 +29,25 @@ def read_initial(input_file):
 
 def quick_test():
     main([None, "test.csv"])
+
+
+def fitness(grid):
+    """Max: 0"""
+    rows = grid
+    cols = np.transpose(grid)
+
+
+def fitness_sub(seq):
+    uniq_elements = np.unique(seq)
+    repeated_number = len(seq) - len(uniq_elements)
+    if 0 in seq:
+        return repeated_number + 1
+    else:
+        return repeated_number
+
+
+def get_sub_grids(grid):
+    pass
 
 
 if __name__ == "__main__":
