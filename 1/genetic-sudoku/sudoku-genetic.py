40474a30b641b61046b3a6d28a35b2c635e4d128
daanturo <daanturo@gmail.com>
2021-04-09 22:01:10 +0700

    1.2: initial population.

--- 1/genetic-sudoku/sudoku-genetic.py
+++ 1/genetic-sudoku/sudoku-genetic.py
@@ -5,22 +5,59 @@ import sys
 import numpy as np
 from dataclasses import dataclass
 
+# TODO: make it configurable
+POPULATION_SIZE = 8
+
+random_generator = np.random.default_rng()
+
 
 @dataclass
 class Grid:
-    def __init__(self, grid):
+    def __init__(self, grid, initial=None):
         self.current = grid
-        self.initial = np.copy(grid)
+        # TODO: shared initial
+        if initial is None:
+            self.initial = np.copy(grid)
+        else:
+            self.initial = initial
         self.N = len(grid)
 
 
 def main(args):
     grid = Grid(read_initial(args[1]))
+    candidates = [
+        Grid(gen_random_grid(grid.initial), grid.initial)
+        for _ in range(POPULATION_SIZE)
+    ]
+    # map(print_grid, candidates)
+    for g in candidates:
+        print_grid(g)
+
+
+def print_grid(grid: Grid):
+    print()
+    for i in range(grid.N):
+        print()
+        for k in range(grid.N):
+            print(
+                grid.current[i][k],
+                " " if grid.initial[i][k] == 0 else ".",
+                "\t",
+                end="",
+            )
+
+
+def gen_random_grid(grid: list[list[int]]):
+    N = len(grid)
+    return [
+        [(random_generator.integers(1, N + 1) if cell == 0 else cell) for cell in row]
+        for row in grid
+    ]
 
 
 def read_initial(input_file):
     """Read initial numbers from input_file
-    Return a list of cell lists; each cell, if initially declared, is that number, else 0"""
+    Return a list of cell lists; each cell, if initially declared, contains that number, else 0"""
     return [
         [(0 if cell == "" else int(cell)) for cell in row]
         for row in csv.reader(open(input_file))
@@ -35,20 +72,28 @@ def fitness(grid):
     """Max: 0"""
     rows = grid
     cols = np.transpose(grid)
+    subs = get_sub_grids(grid)
+    rows_fitness = sum(fitness_sub(row) for row in rows)
+    cols_fitness = sum(fitness_sub(col) for col in cols)
+    subs_fitness = sum(fitness_sub(sub) for sub in subs)
+    return rows_fitness + cols_fitness + subs_fitness
 
 
 def fitness_sub(seq):
     uniq_elements = np.unique(seq)
-    repeated_number = len(seq) - len(uniq_elements)
+    repeated_amount = len(uniq_elements) - len(seq)
     if 0 in seq:
-        return repeated_number + 1
+        return repeated_amount - 1
     else:
-        return repeated_number
+        return repeated_amount
 
 
 def get_sub_grids(grid):
-    pass
+    # TODO: implement
+    return [0]
 
+
+# quick_test()
 
 if __name__ == "__main__":
     main(sys.argv)
