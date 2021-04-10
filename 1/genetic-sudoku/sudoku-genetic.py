ca75d18383bedae9065ed6e9087dd334240f4067
daanturo <daanturo@gmail.com>
2021-04-11 01:12:37 +0700

    1.2: BREAKAGE! Make "crossover" point on [1,N-2].
    Other changes:
    * Data structures
    * Share N and initial grid
    * Various parameters
    * Add .json debug template

--- 1/genetic-sudoku/sudoku-genetic.py
+++ 1/genetic-sudoku/sudoku-genetic.py
@@ -1,131 +1,127 @@
 #!/usr/bin/env python3
 
+from functools import reduce
 import csv
 import sys
 import numpy as np
 from dataclasses import dataclass
 import random
-
-# TODO: make it configurable
-POPULATION_SIZE = 1024
+import operator
 
 random_generator = np.random.default_rng()
 
 
 @dataclass
 class Grid:
-    def __init__(self, grid: list[list[int]], initial=None):
-        self.current = np.copy(grid)
-        # TODO: shared initial, N, max_score
-        if initial is None:
-            self.initial = np.copy(grid)
-        else:
-            self.initial = initial
-        self.N = len(grid)
+    data: list[list[int]]
+
+    def to_string(self, init: list[list[int]]):
+        N = len(self.data)
+        rows_to_strs = [
+            reduce(
+                operator.add,
+                [
+                    str(self.data[row][col])
+                    + (" " if init[row][col] == 0 else ".")
+                    + "\t"
+                    for col in range(N)
+                ],
+            )
+            + "\n"
+            for row in range(N)
+        ]
+        return reduce(operator.add, rows_to_strs)
+
+
+@dataclass
+class Population:
+    def __init__(self, initial: list[list[int]], population_size: int):
+        self.initial = Grid(initial)
+        self.N = len(initial)
+        self.sqrtN = int(np.sqrt(self.N))
         # There are N^2 cells, each can take -1 penalty from : row, column and sub grid constraints
         self.max_score = 3 * (self.N ** 2)
-        self.sqrtN = int(np.sqrt(self.N))
+        self.population_size = population_size
+        # Generate initial population
+        self.candidates = [
+            Grid(gen_random_grid(self.initial)) for _ in range(self.population_size)
+        ]
 
 
 def main(args):
     # Read the partially filled grid from file
-    grid = Grid(read_initial(args[1]))
-    # Generate initial population
-    candidates = [
-        Grid(gen_random_grid(grid.initial), grid.initial)
-        for _ in range(POPULATION_SIZE)
-    ]
+    population = Population(read_initial(args[1]), int(args[2]))
     # Weight each candidates
-    fitness_weights = list(map(fitness, candidates))
+    fitness_weights = list(map(fitness, population.candidates))
     # Record current generation's NO.
     generation_no = 0
     # Loop until a perfect solution is found
-    while not (grid.max_score in fitness_weights):
+    while not (population.max_score in fitness_weights):
 
         # Print info
         print("\n\nGeneration: ", generation_no)
-        for i in range(POPULATION_SIZE):
-            print_grid(candidates[i])
+        for i in range(population.population_size):
+            print(population.candidates[i].to_string(population.initial.data))
             print("Score: ", fitness_weights[i])
 
         # Randomly select candidates by fitness weights
         selected = random.choices(
-            population=candidates, weights=fitness_weights, k=POPULATION_SIZE
+            population=population.candidates,
+            weights=fitness_weights,
+            k=population.population_size,
         )
         # Breed selected candidates
         children = crossover(selected)
         # Mutate the children and have them replace candidates
-        candidates = list(map(mutate, children))
+        population.candidates = [
+            mutate(child, population.initial) for child in children
+        ]
         # Re-calculate fitness weights
-        fitness_weights = list(map(fitness, candidates))
+        fitness_weights = list(map(fitness, population.candidates))
         generation_no += 1
 
     # Get the perfect solution and display it
-    solution = candidates[fitness_weights.index(grid.max_score)]
-    print_grid(solution)
+    solution = population.candidates[fitness_weights.index(population.max_score)]
+    print(solution.to_string(population.initial.data))
 
 
-def crossover(grids: list[Grid], children: list[Grid] = []) -> list[Grid]:
-    """Perform "crossover" step for each pair in grids.
+def crossover(candidates: list[Grid], children: list[Grid] = []) -> list[Grid]:
+    """Perform "crossover" step for each pair in candidates.
     Return the accumulated children."""
-    if grids == []:
+    if candidates == []:
         return children
     else:
-        N = grids[0].N
+        N = len(candidates[0].data)
         # Identify the line to crossover
-        point = random_generator.integers(N - 1)
+        point = random_generator.integers(1, N - 1)
         # Crossover: split at `point`, swap [0,point) and [point,N)
+        # normal list concatenation does not work on numpy matrices
         child0 = Grid(
-            np.concatenate((grids[0].current[0:point], grids[1].current[point:N])),
-            grids[0].initial,
+            np.concatenate((candidates[0].data[0:point], candidates[1].data[point:N]))
         )
         child1 = Grid(
-            np.concatenate((grids[1].current[0:point], grids[0].current[point:N])),
-            grids[0].initial,
+            np.concatenate((candidates[1].data[0:point], candidates[0].data[point:N]))
         )
-        return crossover(grids[2:], children + [child0, child1])
+        return crossover(candidates[2:], children + [child0, child1])
 
 
-def mutate(grid: Grid) -> Grid:
-    """Perform "mutate" on grid."""
+def mutate(grid: Grid, initial: Grid) -> Grid:
+    """Perform "mutate" on grid, leave initial cells intact."""
     # Randomly select a row and column
-    row = random_generator.integers(grid.N)
-    col = random_generator.integers(grid.N)
+    row = random_generator.integers(len(grid.data))
+    col = random_generator.integers(len(grid.data))
     # Replace with a random value unless the cell is immutable
-    if grid.initial[row][col] == 0:
-        # sub_grid_values = get_sub_grids(grid.current)[sub_grid_id(row, col, grid.sqrtN)]
-        # filled_values = set(
-        # grid.current[row] + np.transpose(grid.current)[col] + sub_grid_values
-        # )
-        # unfilled_values = list(set(range(grid.N)) - filled_values)
-        # if unfilled_values == []:
-        # grid.current[row][col] = random_generator.integers(1, grid.N + 1)
-        # else:
-        # grid.current[row][col] = random_generator.choice(unfilled_values)
-        grid.current[row][col] = random_generator.integers(1, grid.N + 1)
+    if initial.data[row][col] == 0:
+        grid.data[row][col] = random_generator.integers(1, len(initial.data) + 1)
     return grid
 
 
-def print_grid(grid: Grid):
-    """Pretty(?) print grid."""
-    print()
-    for i in range(grid.N):
-        for k in range(grid.N):
-            print(
-                grid.current[i][k],
-                " " if grid.initial[i][k] == 0 else ".",
-                "\t",
-                end="",
-            )
-        print()
-
-
-def gen_random_grid(grid: list[list[int]]):
+def gen_random_grid(init: Grid):
     """Return a random grid from the argument by randomly fill all mutable cells."""
-    N = len(grid)
+    N = len(init.data)
     return [
         [(random_generator.integers(1, N + 1) if cell == 0 else cell) for cell in row]
-        for row in grid
+        for row in init.data
     ]
 
 
@@ -140,32 +136,29 @@ def read_initial(input_file):
 
 def quick_test():
     """For REPL interaction."""
-    main([None, "test.csv"])
+    main([None, "test.csv", "1024"])
 
 
 def fitness(grid: Grid):
     """Determine grid's fitness score."""
-    rows = grid.current
+    rows = grid.data
     cols = np.transpose(rows)
     subs = get_sub_grids(rows)
     # These three are negative
     rows_fitness = sum(fitness_sub(row) for row in rows)
     cols_fitness = sum(fitness_sub(col) for col in cols)
     subs_fitness = sum(fitness_sub(sub) for sub in subs)
-    # Ensure positiveness by adding with the maximum score
-    return rows_fitness + cols_fitness + subs_fitness + grid.max_score
+    return rows_fitness + cols_fitness + subs_fitness
 
 
 def fitness_sub(seq: list):
-    """Return 0 - (negative) the number duplicate elements (excluding the first to be
-    duplicated one) and 0s in seq.
-    Since 0 cells are unfilled cells."""
+    """Return the number of unique elements in seq, excluding 0s since 0 cells are
+    unfilled cells. The return value is positive."""
     uniq_elements = np.unique(seq)
-    neg_repeated_amount = len(uniq_elements) - len(seq)
     if 0 in seq:
-        return neg_repeated_amount - 1
+        return len(uniq_elements) - 1
     else:
-        return neg_repeated_amount
+        return len(uniq_elements)
 
 
 def get_sub_grids(mat: list[list[int]]) -> list[list[int]]:
@@ -182,12 +175,12 @@ def get_sub_grids(mat: list[list[int]]) -> list[list[int]]:
 def sub_grid_id(row, col, sqrt_N):
     """Return the appropriate ID of sub grid that cell at row, col would land, given N.
     For example:
-0 	0 	0 	1 	1 	1 	2 	2 	2
-0 	0 	0 	1 	1 	1 	2 	2 	2
-    ...
-3 	3 	3 	4 	4 	4 	5 	5 	5
-    ...
-6 	6 	6 	7 	7 	7 	8 	8 	8 	"""
+    0   0   0   1   1   1   2   2   2
+    0   0   0   1   1   1   2   2   2
+        ...
+    3   3   3   4   4   4   5   5   5
+        ...
+    6   6   6   7   7   7   8   8   8"""
     return (row // sqrt_N) * sqrt_N + col // sqrt_N
 
 
