fb8b511c1ca34d380c169186e7e78a13e86322a6
Daanturo <daanturo@gmail.com>
2021-05-18 16:18:43 +0700

    Define board_after_move()
    Rename variables, change winner -> get_winner and rework

--- ass2/CoGanh.py
+++ ass2/CoGanh.py
@@ -13,14 +13,14 @@ INITIAL_BOARD = [
 
 
 def generate_all_moves(position):
-    goes_in_eight_directions = (position[0] + position[1]) % 2 == 0
+    can_go_in_8_directions = (position[0] + position[1]) % 2 == 0
     unbounded_moves = [
         (position[0] + i, position[1] + j)
         for i in [-1, 0, 1]
         for j in [-1, 0, 1]
-        if ((i != 0 or j != 0) and (goes_in_eight_directions or i * j == 0))
+        if ((i != 0 or j != 0) and (can_go_in_8_directions or i * j == 0))
     ]
-    return [(i, j) for (i, j) in unbounded_moves if (i >= 0 and j >= 0)]
+    return [(row, col) for (row, col) in unbounded_moves if (row >= 0 and col >= 0)]
 
 
 def generate_legal_moves(position, board):
@@ -28,15 +28,22 @@ def generate_legal_moves(position, board):
     return [(i, j) for (i, j) in moves if board[i][j] == 0]
 
 
-def winner(board):
-    flatten_board = np.array(board).flatten()
-    pO_on_board = 1 in flatten_board
-    pX_on_board = -1 in flatten_board
+def get_winner(board):
+    pO_on_board = True in [(1 in row) for row in board]
+    pX_on_board = True in [(-1 in row) for row in board]
     if pO_on_board and not pX_on_board:
         return 1
     if pX_on_board and not pO_on_board:
         return -1
     return 0
+
+
+def board_after_move(source, dest, board):
+    board[source[0]][source[1]], board[dest[0]][dest[1]] = (
+        board[dest[0]][dest[1]],
+        board[source[0]][source[1]],
+    )
+    return board
 
 
 def move(board, player):
