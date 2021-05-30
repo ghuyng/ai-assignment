e5b5aabfe372ca03cf8eed5a46377f1cfff38e3c
Daanturo <daanturo@gmail.com>
2021-05-30 20:46:36 +0700

    Định nghĩa hàm tạo các vị trí gánh được từ một vị trí

--- ass2/CoGanh.py
+++ ass2/CoGanh.py
@@ -4,6 +4,8 @@ import numpy as np
 from functools import reduce
 import operator
 
+import time, copy, random
+
 from typing import List, Tuple
 
 INITIAL_BOARD = [
@@ -13,6 +15,9 @@ INITIAL_BOARD = [
     [-1, 0, 0, 0, -1],
     [-1, -1, -1, -1, -1],
 ]
+
+# Whether the chess piece at [i][j] is able to travel diagonally
+DIAGONAL_OR_CROSSES_ONLY = [[(i + j) % 2 == 0 for i in range(5)] for j in range(5)]
 
 
 def board_to_string(board):
@@ -59,7 +64,67 @@ def board_after_move(source, dest, board):
     return board
 
 
+def gen_opposite_position_pairs(
+    position: Tuple[int, int]
+) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+    """Return a list of pairs of opposing positions around the argument."""
+    adjacent_positions = generate_all_moves(position)
+    x, y = position
+
+    def opposite_pair(adj_pos):
+        opp_of_adj_pos = (2 * x - adj_pos[0], 2 * y - adj_pos[1])
+        if not (opp_of_adj_pos in adjacent_positions):
+            return None
+        else:
+            # Avoid repetition of opposing pairs
+            is_repeated = adjacent_positions.index(adj_pos) > adjacent_positions.index(
+                opp_of_adj_pos
+            )
+            if is_repeated:
+                return None
+            else:
+                return (adj_pos, opp_of_adj_pos)
+
+    return [
+        opposite_pair(pos)
+        for pos in adjacent_positions
+        if opposite_pair(pos) is not None
+    ]
+
+
+# Pairs of positions that the chess piece at [i][j] can potentially "ganh", regardless of "colors"
+# List[List[ List[Tuple[Tuple,Tuple]] ]]
+OPPOSITE_POSITION_PAIRS = [
+    [gen_opposite_position_pairs((i, j)) for j in range(5)] for i in range(5)
+]
+
+
+def ganh(new_position, board, player) -> List[Tuple[Tuple, Tuple]]:
+    """Tra ve danh sach cac cap vi tri quan doi phuong ma neu player di vao new_position thi co the ganh."""
+    pairs = OPPOSITE_POSITION_PAIRS[new_position[0]][new_position[1]]
+    opponent = -player
+
+    def cap_quan_bi_ganh(pair):
+        x0, y0 = pair[0]
+        x1, y1 = pair[1]
+        if board[x0][y0] == board[x1][y1] == opponent:
+            return pair
+        return None
+
+    return [pair for pair in [cap_quan_bi_ganh(_) for _ in pairs] if pair is not None]
+
+
+# TEST_BOARD = [-1 for i in range(8)] + [1 for i in range(8)] + [0 for i in range(9)]
+TEST_BOARD = [
+    [0, 1, 0, -1, 0],
+    [0, -1, 1, 1, 1],
+    [1, -1, -1, -1, 1],
+    [-1, 0, 0, 0, 1],
+    [-1, 0, 0, 1, -1],
+]
+
+
 def move(
     board: List[List[int]], player: int
 ) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-    return ((-1, -1), (-1, -1))
+    return None
