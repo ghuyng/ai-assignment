89536235741e1edfa8ebca66dff7e9df49782e20
Daanturo <daanturo@gmail.com>
2021-08-02 22:53:18 +0700

    Fix gen.sh

--- gen.sh
+++ gen.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 mkdir -p data
 
-for i in {$1..$2}; do
+for i in $(seq $1 $2); do
 	out=data/round${i}.txt
 	# Generate the round if not already exists
 	[[ -f out ]] || python -m main >>data/round${i}.txt
