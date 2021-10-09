1d1ba0adc9ff85e02b56fdf2a095ed1245cb9927
Daanturo <daanturo@gmail.com>
2021-10-09 15:18:15 +0700

    what?

--- gen.sh
+++ gen.sh
@@ -5,6 +5,6 @@ mkdir -p data
 
 for i in $(seq $1 $2); do
 	out=data/round${i}.txt
-	# Generate the round if not already exists
-	[[ -f out ]] || python -m main >>data/round${i}.txt
+	# Generate the round when it doesn't exists
+	[[ -f out ]] || python -m main >>$out
 done
